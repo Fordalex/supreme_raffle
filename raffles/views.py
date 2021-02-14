d5e51c760889483480883d237a42b7fe313d17aa
alex <alex96ford19@gmail.com>
2021-02-14 14:52:05 +0000

    Fixed carousel and moved chairty product to the raffle info page.

--- raffles/views.py
+++ raffles/views.py
@@ -6,11 +6,9 @@ from functions import get_charity_product
 def raffle_page(request):
 
     products = Product.objects.filter(charity=False)
-    charity_product = get_charity_product()
 
     context = {
         'products': products,
-        'charity_product': charity_product,
     }
 
     return render(request, 'raffles/raffles.html', context)
@@ -18,9 +16,11 @@ def raffle_page(request):
 def raffle_info_page(request, pk):
 
     product = Product.objects.get(pk=pk)
+    charity_product = get_charity_product()
 
     context = {
         'product': product,
+        'charity_product': charity_product,
     }
 
     return render(request, 'raffles/raffle_info.html',context)

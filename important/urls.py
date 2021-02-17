d6c3bd8ce8e805e58ac9bf1e5e08035721fe6f51
alex <alex96ford19@gmail.com>
2021-02-17 20:36:32 +0000

    Adding the content for the terms and conditions and privacy policy.

--- important/urls.py
+++ important/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("t&c", views.t_and_c_page, name="t_and_c_page"),
+    path("t&c/", views.t_and_c_page, name="t_and_c_page"),
+    path("privacy_policy/", views.privacy_policy_page, name="privacy_policy_page"),
 ] 
--- important/views.py
+++ important/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def t_and_c_page(request):
     return render(request, 'important/t_and_c.html')
+
+def privacy_policy_page(request):
+    return render(request, 'important/privacy_policy.html')
